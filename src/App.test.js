666c8d1212e2a35b4fe53a0424e11a4f69f14f7c
jamiepika <71554903+jamiepika@users.noreply.github.com>
2021-08-19 14:31:52 +0200

    simple test created

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Loudly Trax app, () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Loudly Trax');
   expect(linkElement).toBeInTheDocument();
 });
