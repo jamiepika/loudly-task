7f190338de910fd4b0cec30e65dda6967e7fa6cc
jamiepika <71554903+jamiepika@users.noreply.github.com>
2021-08-19 14:30:55 +0200

    PlayButton component created

---
+++ src/components/SongCards/PlayButton.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'; 
+
+const PlayButton = (props) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [audio] = useState(new Audio(props.file));
+
+    const togglePlay = () => {
+        setIsPlaying(!isPlaying);
+    };
+
+    useEffect(() => {
+        isPlaying ? audio.play() : audio.pause();
+    }, [isPlaying]);
+
+    return (
+        <button
+        onClick={() => togglePlay()} > { isPlaying ? "Pause" : "Play" } 
+        </button>
+    )
+}
+
+export default PlayButton;
