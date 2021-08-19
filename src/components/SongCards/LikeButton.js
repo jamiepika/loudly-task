aabcb87267c6dab955c66639da602e16b92bae2b
jamiepika <71554903+jamiepika@users.noreply.github.com>
2021-08-19 14:31:15 +0200

    LikeButton component created, POST difficulty WIP

--- src/components/SongCards/LikeButton.js
+++ src/components/SongCards/LikeButton.js
@@ -1,41 +1,39 @@
 import React, {useState} from 'react'; 
 import axios from 'axios'; 
 
+const API_KEY = process.env.REACT_APP_LOUDLY_API_KEY;
+
 const LikeButton = (props) => {
     const [isLiked, setIsLiked] = useState(false);
     const [likeCount, setLikeCount] = useState(0);
+    const id = props.id; 
     
     function likeClicked () {
       setIsLiked(true);
       setLikeCount(likeCount + 1);
-      console.log(likeCount);
-    };
-
-    function postLike (props) { 
-      const queryParam = "apikey=___agAFTxkmMIWsmN9zOpM_6l2SkZPPy21LGRlxhYD8";
-      const bodyParam = props; //track ID
-      const url = `https://api-stg.jam-community.com/interact/like/${queryParam}`;  
-
-      axios
-        .post(url, 
-          { "Likes" : `${likeCount}`}, 
-          {
-          headers: {
+   
+      const queryParam = `apikey=${API_KEY}`;
+      const bodyParam = {
+        'ID': id }
+      const url = `https://api-stg.jam-community.com/interact/like?${queryParam}`;  
+      const headers = {
             'Content-Type' : 'application/x-www-form-urlencoded',
-            'body' : bodyParam
-          }
-        }
+          };
+
+    axios
+        .post(url, bodyParam, {headers}
     ).then((response) => {
         console.log(response);
+        setLikeCount(response);
     }).catch(err => {
             console.log(err)
-        }, []);
-  }
+        });
+  };
+
   return (
         <button 
-        onClick={() => likeClicked()}
-        onClick={() => postLike(props.id)}>
-           Like 
+        onClick={() => likeClicked()}>
+           Likes: {likeCount}
            </button>
     )
 }
